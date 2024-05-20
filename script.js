270844f3ecfcaf3ab03aa4c570379e2b4f07e7dc
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-20 18:13:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let data = document.cookie.split('; ')
+let fontsize = document.getElementById("fontsize")
+let fontcolor = document.getElementById("fontcolor")
+for (let item of data) {
+	let [key, value] = item.split("=")
+	if (key==='fontsize') {
+		alert(value)
+		fontsize.value = value
+	}
+	if (key==='fontcolor') {
+		fontcolor.value = value
+	}
+}
+let submit = document.querySelector("input[type='submit'][value='Save']")
+submit.addEventListener('click', () => {
+	document.cookie = `fontsize=${fontsize.value}; expires=Thu, 18 Dec 2024 12:00:00 UTC; path=/`;
+	document.cookie = `fontsize=${fontcolor.value}; expires=Thu, 18 Dec 2024 12:00:00 UTC; path=/`;
+})
